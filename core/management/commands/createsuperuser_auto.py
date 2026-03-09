a24363f983986dfce7e1a40f857f4fc83c18a720
vaibhav.sharma01 <vaibhav.sharma01@external-travclan.com>
2026-03-09 17:39:56 +0530

    done

---
+++ core/management/commands/createsuperuser_auto.py
@@ -0,0 +1,16 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(f'Superuser {username} created')
+        else:
+            self.stdout.write(f'Superuser {username} already exists')
